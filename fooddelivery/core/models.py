e5144449400d65094f2961128cc5f5846d7d246f
“Drmingler” <kayymedia@gmail.com>
2020-05-08 17:55:58 +0200

    feat : Add entity classes

--- fooddelivery/core/models.py
+++ fooddelivery/core/models.py
@@ -1,6 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
 
-# class User(models.Model):
-#     user =
+class Customer(models.Model):
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=250, verbose_name="First name")
+    last_name = models.CharField(max_length=250, verbose_name="Last name")
+    phone_number = models.CharField(max_length=12, blank=True)
+
+
+class Address(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    city_id = models.ForeignKey("City", on_delete=models.CASCADE)
+    street = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20, blank=True)
+
+
+class Country(models.Model):
+    country_name = models.CharField(max_length=50)
+
+
+class City(models.Model):
+    country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
+    city_name = models.CharField(max_length=50)
+
+
+class Food(models.Model):
+    food_name = models.CharField(max_length=250)
+    food_description = models.TextField()
+    price = models.FloatField()
+
+
+class OrderDetails(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_type = models.CharField(max_length=30, default="Delivery")
+    payment_method = models.CharField(max_length=30, default="Card")
+    order_date = models.DateField(auto_now_add=True)
+    order_time = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order_details_id = models.ForeignKey(OrderDetails, on_delete=models.CASCADE)
+    food_id = models.ForeignKey(Food, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
